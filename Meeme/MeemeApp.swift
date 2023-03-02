e3c5b6530bb8bab15d12b4b5f7ef6ca3ed636247
David Venegas <venegasdavidm@gmail.com>
2023-03-01 21:53:24 -0500

    Initial Commit

---
+++ Meeme/MeemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeemeApp.swift
+//  Meeme
+//
+//  Created by David Venegas on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MeemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
